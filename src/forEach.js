23276ab102bea574caff858a289c22ae6c5847ba
Yair <hello@agzeri.com>
2018-05-24 14:45:51 -0500

    [docs] Add two more code comments to `forEach` implementation.

--- src/forEach.js
+++ src/forEach.js
@@ -1,5 +1,6 @@
 /**
  * @function
+ * @file forEach.js
  *
  * @name forEach
  * @description Iterate through the list and call the function on each element.
@@ -15,7 +16,7 @@
  * forEach([1, 2, 3], number => number * number)
  *
  * @author Mr R <@agzeri>
- * @copyright Mr R 2018
+ * @copyright Ramdish, for JavaScript humans. 2018
  */
 export default (list, fn) => {
   for(let i = 0; i < list.length; i++) {
